034c4ef95b861eb19cc942f5913eaae98b8c6663
vinamra28 <vinjain@redhat.com>
2020-10-07 20:43:05 +0530

    Add Grid for SortByFilter

--- ui/src/components/App/App.tsx
+++ ui/src/components/App/App.tsx
@@ -5,7 +5,9 @@ import CatalogFilter from '../CatalogFilter/CatalogFilter';
 import KindFilter from '../KindFilter/KindFilter';
 import SortByFilter from '../SortByFilter/SortByFilter';
 import { IResourceStore } from '../../store/resources';
-import { Text, TextVariants } from '@patternfly/react-core';
+import { Text, TextVariants, Grid, GridItem, Button } from '@patternfly/react-core';
+import TimesIcon from '@patternfly/react-icons/dist/js/icons/times-icon';
+import '@patternfly/react-core/dist/styles/base.css';
 
 interface store {
   store: IResourceStore;
@@ -13,34 +15,54 @@ interface store {
 
 const App = observer(({ store }: store) => (
   <div className="App">
-    <b
+    <Grid
+      hasGutter
       style={{
-        fontSize: '1.1em',
-        verticalAlign: '-0.2em',
-        color: '#484848',
-        marginLeft: '3em',
-        backgroundColor: 'white'
-      }}
-    >
-      Sort
-    </b>
-    <SortByFilter store={store} />
-    <Text
-      component={TextVariants.h1}
-      style={{
-        fontWeight: 'bold',
-        margin: '4.5em',
-        fontSize: '1.1em',
-        color: '#484848',
-        marginBottom: '-1.1em',
         marginTop: '5em'
       }}
     >
-      Refine By:
-    </Text>
-    <CatalogFilter store={store.catalogStore} />
-    <KindFilter store={store.kindStore} />
-    <CategoryFilter store={store.categoryStore} />
+      <GridItem span={1} rowSpan={3}>
+        <b
+          style={{
+            fontSize: '1.1em',
+            verticalAlign: '-0.2em',
+            color: '#484848',
+            marginLeft: '3em',
+            backgroundColor: 'white'
+          }}
+        >
+          Sort
+        </b>
+      </GridItem>
+      <GridItem span={1}>
+        <SortByFilter store={store} />
+      </GridItem>
+      <Grid rows={2} rowSpan={2}>
+        <GridItem rowSpan={2}>
+          <Text
+            component={TextVariants.h1}
+            style={{
+              fontWeight: 'bold',
+              margin: '4.5em',
+              fontSize: '1.1em',
+              color: '#484848',
+              marginBottom: '-2.0em',
+              marginTop: '5em'
+            }}
+          >
+            Refine By:
+          </Text>
+        </GridItem>
+        <GridItem span={1} rowSpan={2}>
+          <Button variant="plain" aria-label="Clear" onClick={store.clearAll}>
+            <TimesIcon />
+          </Button>
+        </GridItem>
+      </Grid>
+      <CatalogFilter store={store.catalogStore} />
+      <KindFilter store={store.kindStore} />
+      <CategoryFilter store={store.categoryStore} />
+    </Grid>
   </div>
 ));
 
